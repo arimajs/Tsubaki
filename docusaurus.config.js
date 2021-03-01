6f59573eae7b85a5207829f4cbdfcf5229ef7ec8
Lioness100 <jchickmm2@gmail.com>
2021-02-28 19:35:43 -0500

    netlify test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Arima Documentation',
   tagline: 'Documentation for Arima: the Discord Music Quiz Bot',
-  url: 'https://arima.fun',
+  url: 'https://tsubaki.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
